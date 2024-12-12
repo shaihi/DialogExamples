bdfcb81de200816fc47c8735e4685393241ca146
shaihi <shaihi@icloud.com>
2024-12-12 13:25:43 +0200

    Date dialog working

--- app/src/main/java/com/shaihi/dialogexamples/MainActivity.java
+++ app/src/main/java/com/shaihi/dialogexamples/MainActivity.java
@@ -1,6 +1,11 @@
 package com.shaihi.dialogexamples;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
+    Button btnDate, btnMulti;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,42 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnDate = findViewById(R.id.btnDate);
+        btnMulti = findViewById(R.id.btnMulti);
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get current date
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                // DatePickerDialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+                                // Set the selected date in TextView
+                                String date = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear;
+                                Toast.makeText(MainActivity.this,date,Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        year, month, day
+                );
+
+                datePickerDialog.show();
+            }
+        });
+        btnMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
